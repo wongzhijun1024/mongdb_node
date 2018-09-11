751ecbce5d309fb394672f11192fdf7108413a55
hwdeveloper <hw_developer@163.com>
2018-09-11 11:07:14 +0800

    更新代码

--- 05/01插入数据.js
+++ 05/01插入数据.js
@@ -4,8 +4,9 @@ var url = "mongodb://localhost:27017/hw";
 MongoClient.connect(url, function(err, db) {
     if (err) throw err;
     var dbo = db.db("db_student");
+
     var myobj = { name: "小明", age: 18 };
-    dbo.collection("table_student").insertOne(myobj, function(err, res) {
+    dbo.collection("table_bird").insertOne(myobj, function(err, res) {
         if (err) throw err;
         console.log("文档插入成功");
         db.close();
